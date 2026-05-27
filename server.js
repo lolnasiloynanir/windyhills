20a27b9907da71a83d521f813c30c830ffc988c8
lolnasiloynanir <rasimvarol35@gmail.com>
2026-05-28 01:34:13 +0300

    Serve index.html on root path
    Add a route to serve index.html on the root path.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server çalışıyor");
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.listen(process.env.PORT || 3000);
